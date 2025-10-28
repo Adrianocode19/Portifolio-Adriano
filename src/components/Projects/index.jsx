4317d5808e2298d701bdebb54ad745227364772f
Adriano Carlos Texeira <adrianoctl20@gmail.com>
2025-10-27 21:36:33 -0400

    feat: Adicionando icones das tecnologias usadas nos projetos

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,7 +1,33 @@
 import style from "./style.module.css";
 import Title from "../Title";
 import db from "./db.json";
-
+import { lazy } from "react";
+const techComponents = {
+  Vue: lazy(() =>
+    import("react-icons/fa").then((mod) => ({ default: mod.FaVuejs }))
+  ),
+  Tailwind: lazy(() =>
+    import("react-icons/ri").then((mod) => ({ default: mod.RiTailwindCssFill }))
+  ),
+  Nuxt: lazy(() =>
+    import("react-icons/si").then((mod) => ({ default: mod.SiNuxtdotjs }))
+  ),
+  React: lazy(() =>
+    import("react-icons/fa").then((mod) => ({ default: mod.FaReact }))
+  ),
+  HTML: lazy(() =>
+    import("react-icons/fa").then((mod) => ({ default: mod.FaHtml5 }))
+  ),
+  CSS: lazy(() =>
+    import("react-icons/fa").then((mod) => ({ default: mod.FaCss3Alt }))
+  ),
+  JavaScript: lazy(() =>
+    import("react-icons/fa").then((mod) => ({ default: mod.FaJs }))
+  ),
+  TypeScript: lazy(() =>
+    import("react-icons/si").then((mod) => ({ default: mod.SiTypescript }))
+  ),
+};
 const Projects = () => {
   const projects = db.projects;
   return (
@@ -22,7 +48,21 @@ const Projects = () => {
               className={style.projectImg}
             />
             <div className={style.projectTextContainer}>
-              <h3 className={style.projectName}>{project.nome}</h3>
+              <div className={style.projectDetails}>
+                <h3 className={style.projectName}>{project.nome}</h3>
+                <div className={style.projectTechnologies}>
+                  {project.tecnologias.map((tech, idx) => {
+                    const TechIcon = techComponents[tech];
+                    if (!TechIcon) return null;
+                    return (
+                      <div key={idx} className={style.iconWrapper}>
+                        <span className={style.tooltip}>{tech}</span>
+                        <TechIcon className={style.icons} />
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
               <p className={style.projectDescription}>{project.descricao}</p>
             </div>
           </a>
